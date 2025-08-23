ccd0bae72775c857df447645d85a0ae60de28600
Bart <bartolomej.kozorog@gmail.com>
2025-08-23 13:42:46 +0200

    fix(nit): rename imports

--- pkg/sources/activities/activity.go
+++ pkg/sources/activities/activity.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"github.com/glanceapp/glance/pkg/sources/activities/types"
 	"github.com/glanceapp/glance/pkg/sources/providers/changedetection"
-	github2 "github.com/glanceapp/glance/pkg/sources/providers/github"
+	"github.com/glanceapp/glance/pkg/sources/providers/github"
 	"github.com/glanceapp/glance/pkg/sources/providers/hackernews"
-	lobsters2 "github.com/glanceapp/glance/pkg/sources/providers/lobsters"
-	mastodon2 "github.com/glanceapp/glance/pkg/sources/providers/mastodon"
+	"github.com/glanceapp/glance/pkg/sources/providers/lobsters"
+	"github.com/glanceapp/glance/pkg/sources/providers/mastodon"
 	"github.com/glanceapp/glance/pkg/sources/providers/reddit"
 	"github.com/glanceapp/glance/pkg/sources/providers/rss"
 )
@@ -16,24 +16,24 @@ func NewActivity(sourceType string) (types.Activity, error) {
 	var a types.Activity
 
 	switch sourceType {
-	case mastodon2.TypeMastodonAccount:
-		a = mastodon2.NewPost()
-	case mastodon2.TypeMastodonTag:
-		a = mastodon2.NewPost()
+	case mastodon.TypeMastodonAccount:
+		a = mastodon.NewPost()
+	case mastodon.TypeMastodonTag:
+		a = mastodon.NewPost()
 	case hackernews.TypeHackerNewsPosts:
 		a = hackernews.NewPost()
 	case reddit.TypeRedditSubreddit:
 		a = reddit.NewPost()
-	case lobsters2.TypeLobstersTag:
-		a = lobsters2.NewPost()
-	case lobsters2.TypeLobstersFeed:
-		a = lobsters2.NewPost()
+	case lobsters.TypeLobstersTag:
+		a = lobsters.NewPost()
+	case lobsters.TypeLobstersFeed:
+		a = lobsters.NewPost()
 	case rss.TypeRSSFeed:
 		a = rss.NewFeedItem()
-	case github2.TypeGithubReleases:
-		a = github2.NewRelease()
-	case github2.TypeGithubIssues:
-		a = github2.NewIssue()
+	case github.TypeGithubReleases:
+		a = github.NewRelease()
+	case github.TypeGithubIssues:
+		a = github.NewIssue()
 	case changedetection.TypeChangedetectionWebsite:
 		a = changedetection.NewWebsiteChange()
 	default:
